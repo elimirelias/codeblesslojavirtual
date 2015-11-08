9e3d5ae6400c54e937b0cb64f82714a57bb915e4
eliaslima <elimirelias@gmail.com>
2015-11-07 23:03:21 -0200

    Initial commit

---
+++ Codebless.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codebless.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestCodebless
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
